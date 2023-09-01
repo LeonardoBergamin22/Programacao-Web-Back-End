da2c91f990d31859357565ada326419fc280f0b0
Leonardo Bergamin <140442294+LeonardoBergamin22@users.noreply.github.com>
2023-09-01 07:57:09 -0300

    Add files via upload

---
+++ ListaDePaises/src/paiscontroller/PaisController.java
@@ -0,0 +1,43 @@
+package paiscontroller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import dao.PaisDAO;
+import paismodel.PaisModel;
+import paisview.PaisView;
+
+public class PaisController {
+	private PaisView paisView;
+	private PaisDAO paisDAO;
+
+	public PaisController() {
+		paisView = new PaisView();
+		paisDAO = new PaisDAO();
+	}
+
+	public void adicionarPais(String nome, String capital) {
+		PaisModel pais = new PaisModel(nome, capital);
+		paisDAO.adicionaPais(pais);
+	}
+
+	public void exibirListaDePaises() {
+		paisView.exibirListaDePaises(paisDAO.getListaDePaises());
+	}
+
+	public void removerDaListaDePaises(String nome) {
+		for (int i = 0; i < paisDAO.getListaDePaises().size(); i++) {
+			if (paisDAO.getListaDePaises().get(i).getNome().equalsIgnoreCase(nome)) {
+				paisDAO.removePais(paisDAO.getListaDePaises().get(i));
+				paisView.exibirRemovido(nome);
+			}
+
+		}
+
+	}
+
+	public void limparLista() {
+		paisDAO.limpaLista();
+		paisView.exibirLimpa();
+	}
+}
