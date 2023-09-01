fb5d654ac452469482b213f3f692a80768599bd4
Leonardo Bergamin <140442294+LeonardoBergamin22@users.noreply.github.com>
2023-09-01 09:36:38 -0300

    Add files via upload

---
+++ CadrastroDePlanetas/src/model/PlanetaModel.java
@@ -0,0 +1,19 @@
+package model;
+
+public class PlanetaModel {
+
+	private String nome;
+	
+	public PlanetaModel(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
