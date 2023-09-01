4c2aaf1406912bae050491bda4c4737183612446
Leonardo Bergamin <140442294+LeonardoBergamin22@users.noreply.github.com>
2023-09-01 08:07:56 -0300

    Add files via upload

---
+++ ListaDePaises/src/view/PaisView.java
@@ -0,0 +1,27 @@
+package view;
+
+import java.util.List;
+
+import model.PaisModel;
+
+public class PaisView {
+	public void exibirListaDePaises(List<PaisModel> paises) {
+		System.out.println("=== Lista de paises ===");
+		System.out.println("-----------------------");
+		System.out.println("                       ");
+
+		for (PaisModel pais : paises) {
+			System.out.println("Pais: " + pais.getNome() + " - Capital: " + pais.getCapital());
+		}
+		System.out.println("--------------------------------");
+		System.out.println("                                ");
+	}
+	
+	public void exibirLimpa() {
+		System.out.println("Lista limpa com sucesso!");
+	}
+	
+	public void exibirRemovido(String nome) {
+		System.out.println("Pais "+nome+" removido com sucesso!");
+	}
+}
